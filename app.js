4e9bc472fdd8a6bdcbe387f3e5b554dd948b7092
Pike1868 <luis_14mendoza@protonmail.com>
2023-05-02 19:57:20 -0400

    fixing js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ async function getGif() {
     const searchTerm = $("#search").val();
     if (!searchTerm) {
       alert("Please enter a search term");
-      return;
     }
 
     const response = await axios.get(`http://api.giphy.com/v1/gifs/search`, {
